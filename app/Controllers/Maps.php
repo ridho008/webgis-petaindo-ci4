16e8f67a3dad72495c2ecfa8c59354ac2077e8d3
ridho008 <ciuyy12@gmail.com>
2021-02-12 17:05:09 +0700

    Load Layer Peta dan Membuat Peta Tematik

--- app/Controllers/Maps.php
+++ app/Controllers/Maps.php
@@ -6,6 +6,29 @@ class Maps extends BaseController
 {
 	public function index()
 	{
-		return view('maps/index');
+      $model = new \App\Models\DataModel();
+      $file = file_get_contents("./maps/prov.geojson");
+      $file = json_decode($file);
+
+      $features = $file->features;
+
+      foreach ($features as $index => $feature) {
+         $kode_wilayah = $feature->properties->kode;
+         $data = $model->where('id_master_data', 1)
+                       ->where('kode_wilayah', $kode_wilayah)
+                       ->first();
+
+         if($data) {
+            $features[$index]->properties->nilai = $data->nilai;
+         }
+      }
+
+      $nilaiMax = $model->select("MAX(nilai) AS nilai")->where('id_master_data', 1)->first()->nilai;
+
+
+		return view('maps/index', [
+         'data' => $features,
+         'nilaiMax' => $nilaiMax
+      ]);
 	}
 }
